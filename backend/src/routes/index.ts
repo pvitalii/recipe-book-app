dc677202c5b0eadbf1f3b77ba47a371530c76989
pvitalii <72783389+pvitalii@users.noreply.github.com>
2023-02-05 12:10:40 +0200

    feat: Add implemented routes into main router file

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,9 +1,19 @@
 import { Application } from 'express';
+import { ROUTE_PATHS } from '../common/consts';
+import categoryRouter from './api/category.route';
+import ingredientRouter from './api/ingredient.route';
+import measureUnitRouter from './api/measure-unit.route';
+import productRouter from './api/product.route';
+import recipeRouter from './api/recipe.route';
 
 export class AppRouter {
   constructor(private app: Application) {}
 
   init() {
-    this.app.get('/', (_req, res) => res.json('API RUNNING'));
+    this.app.use(`${ROUTE_PATHS.BASE}/recipe`, recipeRouter);
+    this.app.use(`${ROUTE_PATHS.BASE}/product`, productRouter);
+    this.app.use(`${ROUTE_PATHS.BASE}/category`, categoryRouter);
+    this.app.use(`${ROUTE_PATHS.BASE}/measure-unit`, measureUnitRouter);
+    this.app.use(`${ROUTE_PATHS.BASE}/ingredient`, ingredientRouter);
   }
 }
